da8072bf58df1407e110cac3dba46119f69a888a
Abhishek Raj <abhishekraj272@gmail.com>
2021-06-15 12:54:19 +0530

    Fixed: ace worker bug/ File was not loading

--- src/index.js
+++ src/index.js
@@ -7,12 +7,29 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react'
 
+import "ace-builds/webpack-resolver";
 import "ace-builds/src-noconflict/mode-html";
 import "ace-builds/src-noconflict/mode-css";
 import "ace-builds/src-noconflict/mode-javascript";
+
 import "ace-builds/src-noconflict/theme-github";
 import "ace-builds/src-noconflict/theme-cobalt";
+
 import "ace-builds/src-noconflict/ext-language_tools";
+import "ace-builds/src-min-noconflict/ext-spellcheck";
+
+import "ace-builds/src-min-noconflict/snippets/javascript";
+import "ace-builds/src-noconflict/snippets/html";
+import "ace-builds/src-noconflict/snippets/css";
+
+import 'ace-builds/src-min-noconflict/ext-searchbox';
+
+const ace = require('ace-builds/src-noconflict/ace');
+ace.config.set("basePath", "https://cdn.jsdelivr.net/npm/ace-builds@1.4.3/src-noconflict/");
+ace.config.setModuleUrl('ace/mode/javascript_worker', "https://cdn.jsdelivr.net/npm/ace-builds@1.4.3/src-noconflict/worker-javascript.js");
+ace.config.setModuleUrl('ace/mode/css_worker', "https://cdn.jsdelivr.net/npm/ace-builds@1.4.3/src-noconflict/worker-css.js");
+ace.config.setModuleUrl('ace/mode/html_worker', "https://cdn.jsdelivr.net/npm/ace-builds@1.4.3/src-noconflict/worker-html.js");
+
 
 ReactDOM.render(
   <React.StrictMode>
