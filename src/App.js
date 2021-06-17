60edd6844697ceb78d5c4168eb9ef8b7f0ef017a
Abhishek Raj <abhishekraj272@gmail.com>
2021-06-17 22:44:23 +0530

    Added react-helmet and analytics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Body from './component/Body/Body';
+import Header from './component/Header/Header';
 import Navbar from './component/Navbar/Navbar';
 
 function App() {
   return (
     <div className="app">
+      <Header />
       <Navbar />
       <Body />
     </div>
